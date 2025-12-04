4cca310f384b13f38c3b4dc3454d78bad2ff17cc
Bahar Kocgunduz <baharkocgunduz2@gmail.com>
2025-12-04 23:43:07 +0300

    basari olasiligini etkileyen fonksiyonlar düzenlendi

--- main.py
+++ main.py
@@ -1,20 +1,10 @@
-'''
-rüzgar için hız ve yön veriliyor.
-x,y,z konumları verilse buna göre konum alanı içine girince sensör devreye girse
-
-'''
-
 from json import load
 import math
 
 toplam_enerji = 100
-enerji_katsayisi = 1.0
-ruzgar = 2
-hiz = 2
-sensor = 1
-x0 , y0, z0 = 0, 0, 0 # parametreler başlangıç konumuu veriyor
+enerji_katsayisi = 1.0 
+x0, y0, z0 = 0, 0, 0 
 
-# json dosyası okundu , liste değişkeni liste türünde hedef bilgilerini taşıyor
 with open("liste.json", "r") as f:
     dosya = load(f)
     liste = dosya["hedefler"]
@@ -25,51 +15,60 @@ with open("basari_kriterleri.json", "r") as f:
      kriterler = dosya["kriterler"]
      f.close()
 
-# liste içinde tutulan x,y,z konumları kullanılarak en kısa mesafeyi hesaplayan fonksiyon
-# hesaplanan mesafeler liste içinde her hedef dict için ekleniyor
 def mesafe_hesaplama(x0, y0 , z0):
     for i in liste:
         x = i["konum_x"]
         y = i["konum_y"]
         z = i["konum_z"]
-        mesafe = math.sqrt((x - x0)**2 + (y - y0)**2 + (z - z0)**2) # en kısa mesafe hesaplanıyor
+        mesafe = math.sqrt((x - x0)**2 + (y - y0)**2 + (z - z0)**2) 
         i["mesafe"] = float(format(mesafe, ".2f"))
 
-# alana girip girmediğini kontrol edecek
-# eğer alana giriyorsa o alan için belirlenen gürültü değeri alınacak
-# sonuçta döndürülecek 
-def g_alan(kriterler, x0, y0):
+def g_alan(kriterler, x, y):
     noise = 0
     for i in kriterler:
         if i["id"] == "sensor_hatasi":
             gurultu_alanlari = i["gurultu_alanlari"]
     for i in gurultu_alanlari:
-        if i["x_min"] < x0 < i["x_max"] and i["y_min"] < y0 < i["y_max"]:
+        if i["x_min"] < x < i["x_max"] and i["y_min"] < y < i["y_max"]:
             noise = i["deger"]
     return noise
 
-# basari_kriterleri içindeki listeyi alıp harcanacak enerji hesaplanıyor
-# kalan_enerji hesaplanıp return ediliyor
-def cevresel_etki(kriterler, kalan_enerji, x0, y0):
-    for i in kriterler:
-        gereken_enerji = 0
-        if i["id"] == "ruzgar":
-            gereken_enerji = i["hiz"]*ruzgar
-        elif  i["id"] == "hiz":
-            gereken_enerji = i["deger"]*hiz
-        elif i["id"] == "sensor_hatasi":
-            deger = g_alan(kriterler, x0, y0)
-            gereken_enerji = deger*sensor
-        kalan_enerji -= gereken_enerji
-    return kalan_enerji
 
-# hesaplanan skorlar liste içinde her hedef dict için ekleniyor
-# liste skor değerlerine göre azalan sırayla sıralanıyor
-# kalan enerji güncelleniyor
-# seçilen hedefler secilen listesine ekleniyor
-# secilen listesi ve son enerji return ediliyor 
+def ruzgar_etkisi(kriterler):
+    ruzgar_degeri = 0
+    ruzgar_onem = 0.8
+    ruzgar_normali = 0
+    for i in kriterler:
+        if i["id"] == "ruzgar":
+            ruzgar_normali = ((i["hiz"]-1)/(10-1))
+    ruzgar_degeri = 1-(ruzgar_onem*ruzgar_normali)
+    return ruzgar_degeri
+
+def gurultu_etkisi(kriterler, x, y):
+    gurultu_degeri = 0
+    sensor = 0.3
+    noise = g_alan(kriterler, x, y)
+    gurultu_degeri = (1-noise)*sensor
+    return gurultu_degeri
+
+def basari_olasiligi(liste):
+    for i in liste:
+        mesafe_degeri = 0
+        hiz = 0.05
+        mesafe_hesaplama(x0, y0, z0)
+        mesafe = i["mesafe"]
+        mesafe_degeri = 1 / (1 + (mesafe*hiz))
+        gx = i["konum_x"]
+        gy = i["konum_y"]
+        gurultu_degeri = gurultu_etkisi(kriterler, gx, gy)
+        ruzgar_degeri = ruzgar_etkisi(kriterler)
+        basari = mesafe_degeri*gurultu_degeri*ruzgar_degeri
+        i["basari_olasiligi"] = basari
+
 
 def skor(liste, toplam_enerji, enerji_katsayisi):
+    global x0, y0, z0
+
     for i in liste:
         i["skor"] = (i["basari_olasiligi"]* i["deger"]) / i["mesafe"]
 
@@ -87,11 +86,9 @@ def skor(liste, toplam_enerji, enerji_katsayisi):
                 z0 = i["konum_z"]
                 mesafe_hesaplama(x0, y0, z0)
                 kalan_enerji -= gereken_enerji
-                kalan_enerji -= cevresel_etki(kriterler, kalan_enerji, x0, y0)
     return secilen, kalan_enerji
 
-
-mesafe_hesaplama(x0, y0, z0)
+basari_olasiligi(liste)
 secilen, kalan_enerji = skor(liste, toplam_enerji, enerji_katsayisi)
 print("Secilen hedefler sirasiyla: ", secilen)
 print("Kalan enerji miktari: ", format(kalan_enerji, ".2f"))
